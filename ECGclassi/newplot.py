512ceb024a6116c93a5bf315c262b23de682dfdc
sanket naik <sanketn7941@gmail.com>
2017-10-07 08:46:02 +0300

    fetch xlxe file data and plot

---
+++ ECGclassi/newplot.py
@@ -0,0 +1,16 @@
+import xlrd
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_location = "/home/pi/Desktop/ECG/normal.xls"
+workbook = xlrd.open_workbook(file_location)
+first_sheet = workbook.sheet_by_index(0)
+
+x = [first_sheet.cell_value(0,i) for i in range(180)]
+#y = [first_sheet.cell_value(i, 1) for i in range(first_sheet.ncols)]
+
+
+plt.plot(x)
+
+
+plt.show()
